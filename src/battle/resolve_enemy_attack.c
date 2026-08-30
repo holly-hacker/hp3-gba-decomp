048e609d8d193b81994b3ba6af92953ee3d6e5bc
Variant9 <dario.db@hotmail.com>
2026-08-30 02:07:21 +0200

    Clean up ResolveEnemyAttack decomp

--- src/battle/resolve_enemy_attack.c
+++ src/battle/resolve_enemy_attack.c
@@ -8,47 +8,40 @@
 // Handle monster attack damage calculation
 s32 ResolveEnemyAttack(s32 attackerIndex, s32 defenderIndex)
 {
-    BattleFighter *pFighters;
     u8 accuracy;
     u16 hitRoll;
     u32 damage;
     u16 critRoll;
 
-    pFighters = g_pFightState->pFighters;
-
-    // Hidden lowers the attacker's effective accuracy by 25.
-    if (pFighters[defenderIndex].bStatusFlags & Hidden)
-        accuracy = pFighters[attackerIndex].bAccuracy - 25;
+    // Fumos lowers the attacker's effective accuracy by 25.
+    if (DEFENDER.bStatusFlags & Hidden)
+        accuracy = ATTACKER.bAccuracy - 25;
     else
-        accuracy = pFighters[attackerIndex].bAccuracy;
+        accuracy = ATTACKER.bAccuracy;
 
+    // Roll for accuracy
     hitRoll = Mt19937RandMax(99);
     if (hitRoll < accuracy)
     {
         damage = Mt19937RandRange(ATTACKER.wDamageRollMin, ATTACKER.wDamageRollMax);
         damage = (damage * DEFENDER.bDefenseFactorPercent) / 100;
 
-        // AttackWeakened (Spongify) and DefenseBoost (Be More Careful) each halve damage, stacking to a quarter.
-        if (ATTACKER.bStatusFlags & AttackWeakened)
+        // Spongify or Hermione's "Be More Careful"
+        if ((ATTACKER.bStatusFlags & AttackWeakened) && (DEFENDER.bStatusFlags & DefenseBoost))
         {
-            // Hermione's "Be More Careful"
-            if ((DEFENDER.bStatusFlags & DefenseBoost))
-            {
-                damage /= 4;
-            }
-            else
-            {
-                damage /= 2;
-                goto halveCheck;
-            }
+            damage /= 4;
         }
         else
         {
-halveCheck:
-            // Hermione's "Be More Careful"
+            // Spongify
+            if (ATTACKER.bStatusFlags & AttackWeakened)
+                damage /= 2;
+
+            // "Be More Careful"
             if (DEFENDER.bStatusFlags & DefenseBoost)
                 damage /= 2;
         }
+
         damage += 1;
     }
     else
@@ -56,13 +49,15 @@ halveCheck:
         damage = 0;
     }
 
-    // Roll for crit if not a miss and target is not hidden
+    // Roll for crit if not a miss and target is not under Fumos
     if (damage != 0 && !(DEFENDER.bStatusFlags & Hidden))
     {
         critRoll = Mt19937RandMax(100);
         if (critRoll > 100 - ATTACKER.bCritChance)
         {
             damage *= 2;
+
+            // Sentinel/marker value
             damage += 999;
         }
     }
